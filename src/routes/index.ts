63b599eebcba1c63da034d2322577a6ecb970cff
Anmol Adhikari <adhikari.anmol97@gmail.com>
2020-05-24 21:33:44 -0500

    ok

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,10 +9,10 @@ import { json } from 'body-parser';
 
 route.get('/',async (req,res)=>{
 
-    res.send(`<h1>Welcome to Superhero API</h1>
+    res.send(`<h1>Hello Deepak Rai</h1>
     
                 <div>From here you can view all the stuffs related to Superhero from the comics aand get their information.</div><br><br>
-                <div>If you are User go to "localhost:3001/user" OR if you are Admin visit "localhost:3001/Admin"</div>
+                <div>If you are User go to "localhost:3009/user" OR if you are Admin visit "localhost:3009/Admin"</div>
             `)
 
 });
