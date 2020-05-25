ada8b5eaaaf6bf1c31bf5d2507452ff5dad38112
Anmol Adhikari <adhikari.anmol97@gmail.com>
2020-05-24 21:58:42 -0500

    commited

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,7 +9,7 @@ import { json } from 'body-parser';
 
 route.get('/',async (req,res)=>{
 
-    res.send(`<h1>Hello Everyone</h1>
+    res.send(`<h1>Hello Everyone 1..2..3..</h1>
     
                 <div>From here you can view all the stuffs related to Superhero from the comics aand get their information.</div><br><br>
                 <div>If you are User go to "localhost:3009/user" OR if you are Admin visit "localhost:3009/Admin"</div>
