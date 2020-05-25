844fd08b485b538b10d570817968bb3ae11f641e
Anmol Adhikari <adhikari.anmol97@gmail.com>
2020-05-24 21:46:05 -0500

    Check

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,11 +7,9 @@ import {Hero} from '../model/hero'
 import { json } from 'body-parser';
 
 
-
-
 route.get('/',async (req,res)=>{
 
-    res.send(`<h1>Hello Deepak Rai</h1>
+    res.send(`<h1>Hello Everyone</h1>
     
                 <div>From here you can view all the stuffs related to Superhero from the comics aand get their information.</div><br><br>
                 <div>If you are User go to "localhost:3009/user" OR if you are Admin visit "localhost:3009/Admin"</div>
