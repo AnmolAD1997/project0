e0d5aa40a5ac034be610ee13f0e9f3d48771e1a7
Anmol Adhikari <adhikari.anmol97@gmail.com>
2020-05-27 16:18:25 -0500

    database

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,11 +2,11 @@ import {Pool} from 'pg';
 
 export const pool= new Pool({
 
-host:"localhost",
+host:"project0.c0ftwtumnrai.us-east-2.rds.amazonaws.com",
 port:5432,
-database:"project0",
-user:"anmol",
-password:"anmol"
+database:"postgres",
+user:"project0",
+password:"project0"
 });
 
 
